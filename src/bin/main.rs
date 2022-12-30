7c41a5d7873e7e688062bbe7b5a8ff58ee2ea87a
Allen Wild <allenwild93@gmail.com>
2022-12-30 16:51:35 -0500

    bin: group results by length and sum

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,4 +1,22 @@
+use std::collections::BTreeMap;
+use std::fmt;
+
 use rustyline::{config::Configurer, error::ReadlineError, Editor};
+
+use killercage::DigitSet;
+
+/// Helper for grouping digitsets. Field order corresponds to sort order
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+struct SetGroup {
+    len: u8,
+    sum: u8,
+}
+
+impl fmt::Display for SetGroup {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "[{} in {}]", self.sum, self.len)
+    }
+}
 
 /// Handle a constraint sentence command
 fn handle_sentence(input: &str) {
@@ -16,25 +34,28 @@ fn handle_sentence(input: &str) {
         return;
     }
 
-    // matching sets will already be sorted by length, group them for better readability.
-    // TODO: find some way to group by both length and sum
-    let mut last_len = sets[0].len();
-    let mut first = true;
-
+    // Group sets by length and sum. The BTreeMap handles ordering by the SetGroup (length and then
+    // sum), and matching_sets already returns results in order so the Vec for each group will be
+    // sorted already.
+    let mut map = BTreeMap::<SetGroup, Vec<DigitSet>>::new();
     for set in sets {
-        if set.len() != last_len {
-            println!();
-            last_len = set.len();
-            first = true;
-        }
-
-        if first {
-            print!("[len {}]", set.len());
-        }
-        print!(" {set}");
-        first = false;
+        let group = SetGroup { len: set.len(), sum: set.sum() };
+        map.entry(group).or_default().push(set);
     }
-    println!();
+
+    for (group, sets) in map {
+        print!("{}{} ", group, if group.sum >= 10 { "" } else { " " });
+        let mut first = true;
+        for set in sets {
+            if first {
+                print!("{set}");
+                first = false;
+            } else {
+                print!(" {set}");
+            }
+        }
+        println!();
+    }
 }
 
 fn main() {
